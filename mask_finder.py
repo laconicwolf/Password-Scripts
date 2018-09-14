677050340697fcda54d1763f6c004f8788ca196a
Jake Miller <laconicwolf@gmail.com>
2018-09-14 17:45:05 -0500

    Add files via upload

--- mask_finder.py
+++ mask_finder.py
@@ -1,48 +1,92 @@
-import sys
-import re
-from pathlib import Path
+#!/usr/bin/env python3
 
-__author__ = 'Jake Miller'
-__date__ = '20171013'
-__version__ = '0.01'
-__description__ = 'Analyzes a password list to find frequency of password character masks'
 
-if len(sys.argv) != 2:
-    print('Usage: mask_finder.py [filename]')
-    exit()    
+__author__ = 'Jake Miller (@LaconicWolf)'
+__date__ = '20180914'
+__version__ = '0.02'
+__description__ = """Reads in one or more plain-text password lists to find the frequency 
+of password character masks. Will display all character masks found"""
 
-print('The bigger the file, the longer it will take...')    
-    
-infile = sys.argv[1]
-outfile = 'masks_out.txt'
 
-if not Path(infile).is_file():
-    print('The file {} could not be found. Please try again'.format(infile))
-    exit()
+import argparse
+import string
+import os
+from collections import Counter
 
-pattern_list = []
 
-with open(infile) as file:
-    for word in file:
-        mask = ''
-        for char in word:
-            if char == '\n':
-                continue
-            elif re.search(r'[A-Z]', char):
-                mask += 'u'
-            elif re.search(r'[a-z]', char):
-                mask += 'l'
-            elif re.search(r'[0-9]', char):
-                mask += 'd'
-            elif re.search(r'\W', char):
-                mask += 's' 
-        pattern_list.append(mask)
-            
+def generate_char_mask(word):
+    """Analyzes the characters of a word and generates a mask per character.
+    Returns the mask of a word as a string."""
+    mask = ''
+    for char in word:
+        try:
+            if char in string.uppercase:
+                mask += '?u'
+            elif char in string.lowercase:
+                mask += '?l'
+            elif char in string.digits:
+                mask += '?d'
+            elif char in string.punctuation:
+                mask += '?s'
+            else:
+                print('Encountered an non-standard character: {}. It will appear in a mask as ?X '.format(char))
+                mask += '?X'
+        except AttributeError:
+            if char in string.ascii_uppercase:
+                mask += '?u'
+            elif char in string.ascii_lowercase:
+                mask += '?l'
+            elif char in string.digits:
+                mask += '?d'
+            elif char in string.punctuation:
+                mask += '?s'
+            else:
+                print('Encountered an non-standard character: {}. It will appear in a mask as ?X '.format(char))
+                mask += '?X'
+    return mask
 
-pattern_dict = {i:pattern_list.count(i) for i in pattern_list}
-sorted_pattern_dict = [(k, pattern_dict[k]) for k in sorted(pattern_dict, key=pattern_dict.get, reverse=True)]
 
-with open(outfile, 'w') as file:
-    for k, v in sorted_pattern_dict:
-        print(k + " : " + str(v))
-        file.write(k + " : " + str(v) + "\n")
+def sort_char_masks(mask_list):
+    """Returns a dictionary of character masks, where the mask is the key and the 
+    value is the number of times the mask occurs
+    """
+    mask_dict = dict(Counter(mask_list))
+    sorted_mask_dict = [(k, mask_dict[k]) for k in sorted(mask_dict, key=mask_dict.get, reverse=True)]
+    return sorted_mask_dict
+
+
+def main():
+    for filename in args.filename:
+        words = open(filename).read().splitlines()
+        masks = [generate_char_mask(word) for word in words]
+        sorted_masks = sort_char_masks(masks)
+        if args.outfile:
+            with open('masks.txt', 'w') as outfile:
+                for k, v in sorted_masks:
+                    print("{} : {}".format(k, v))
+                    outfile.write("{} : {}\n".format(k, v))
+        else:
+            for k, v in sorted_masks:
+                print("{} : {}".format(k, v))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename",
+                        nargs='*',
+                        help="Specify a file containing the output of an nmap scan in xml format.")
+    parser.add_argument("-o", "--outfile",
+                        action="store_true",
+                        help="Writes the output to a file in the current directory.")
+    args = parser.parse_args()
+
+    if not args.filename:
+        parser.print_help()
+        print("\n[-] Please specify one or more password files to analyze. For multiple input files, separate with a space.\n")
+        exit()
+    for filename in args.filename:
+        if not os.path.exists(filename):
+            parser.print_help()
+            print("\n[-] The file {}, does not exist or you lack permissions to open it. Please try again.\n".format(filename))
+            exit()
+    main()

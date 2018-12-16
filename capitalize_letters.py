780e16810a486b7c3551fc95f0f742f33930e5ec
Jake Miller <laconicwolf@gmail.com>
2018-12-16 12:53:40 -0600

    Added option to capitalize all letters.

--- capitalize_letters.py
+++ capitalize_letters.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
-
 __author__ = 'Jake Miller (@LaconicWolf)'
 __date__ = '20180924'
 __version__ = '0.01'
 __description__ = """Convert all words to lowercase and capitalizes letters at 
 specified indexes. Can write output to a new file."""
 
-
 import argparse
 import os
-
 
 def main():
     print('[*] Reading file: {}...'.format(filename))
@@ -19,7 +16,10 @@ def main():
     print('[*] Processing {} words...'.format(len(words)))
     print('[*] Changing all words to lowercase...')
     words = [word.lower() for word in words]
-    if args.index:
+    if args.all:
+        print('[*] Capitalizing all letters...')
+        capitalized_words = [word.upper() for word in words]
+    elif args.index:
         print('[*] Capitalizing letters at specified indexes...')
         capitalized_words = []
         for word in words:
@@ -43,21 +43,20 @@ def main():
         for word in capitalized_words:
             print(word)
 
-
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--filename",
                         help="Specify a file containing words.")
     parser.add_argument("-o", "--outfile",
                         help="Writes the output to a specified file.")
+    parser.add_argument("-a", "--all",
+                        action="store_true",
+                        help="Capitalizes all letters.")
     parser.add_argument("-i", "--index",
                         nargs='*',
                         type=int,
                         help="Specify one or more integers to be used as positions (zero index) to capitalize a letter or letters.")
     args = parser.parse_args()
-
     if not args.filename:
         parser.print_help()
         print("\n[-] Please specify an input file containing words (-f).\n")

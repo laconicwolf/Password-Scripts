1a1cbf3dd5bb6dbe949cebbdc96577e0f7b1db66
Jake Miller <laconicwolf@gmail.com>
2019-03-16 14:30:03 -0500

    Update date_rule.py

--- date_rule.py
+++ date_rule.py
@@ -25,23 +25,23 @@ def create_2_digit_year():
         string_year_range.append(i)
     return string_year_range
 
-def create_month_day():
+def create_month_day(start_year, end_year):
     """Returns a list of from 0101-1231."""
     month_day_list = []
-    times = pd.date_range('01-01-2020', '12-31-2021')
+    times = pd.date_range('01-01-{}'.format(start_year), '12-31-{}'.format(end_year))
     for time in times:
         month_day_list.append(''.join(str(time).split(' ')[0].split('-')[1:]))
-    return month_day_list
+    return list(set(month_day_list))
 
-def create_month_day_2_dig_year():
+def create_month_day_2_dig_year(start_year, end_year):
     """Returns a list of from 010100-123199."""
     month_day_year = []
-    times = pd.date_range('01-01-2000', '12-31-2099')
+    times = pd.date_range('01-01-{}'.format(start_year), '12-31-{}'.format(end_year))
     for time in times:
         year = str(time).split('-')[0][-2:]
         month_day = ''.join(str(time).split(' ')[0].split('-')[1:])
         month_day_year.append(month_day + year)
-    return month_day_year
+    return list(set(month_day_year))
 
 def create_month_day_4_dig_year(start_year, end_year):
     """Returns a list of from 01011900-12312099."""
@@ -51,9 +51,9 @@ def create_month_day_4_dig_year(start_year, end_year):
         year = ''.join(str(time).split('-')[0])
         month_day = ''.join(str(time).split(' ')[0].split('-')[1:])
         month_day_year.append(month_day + year)
-    return month_day_year
+    return list(set(month_day_year))
 
-def create_2_dig_year_month_day():
+def create_2_dig_year_month_day(start_year, end_year):
     """Returns a list of from 000101-991231."""
     year_month_day = []
     times = pd.date_range('01-01-2000', '12-31-2099')
@@ -61,7 +61,7 @@ def create_2_dig_year_month_day():
         year = str(time).split('-')[0][-2:]
         month_day = ''.join(str(time).split(' ')[0].split('-')[1:])
         year_month_day.append(year + month_day)
-    return year_month_day
+    return list(set(year_month_day))
 
 def create_4_dig_year_month_day(start_year, end_year):
     """Returns a list of from 19000101-20991231."""
@@ -69,7 +69,7 @@ def create_4_dig_year_month_day(start_year, end_year):
     times = pd.date_range('01-01-{}'.format(start_year), '12-31-{}'.format(end_year))
     for time in times:
         year_month_day.append(str(time).split(' ')[0].replace('-', ''))
-    return year_month_day
+    return list(set(year_month_day))
 
 def main():
     print("[*] Generating dates...")
@@ -78,23 +78,21 @@ def main():
     two_dig_year = create_2_digit_year()
 
     # 0101-1231
-    month_day = create_month_day()
+    month_day = create_month_day(start_year, end_year)
 
     # 010100-123199
-    month_day_2_dig_year = create_month_day_2_dig_year()
+    month_day_2_dig_year = create_month_day_2_dig_year(start_year, end_year)
 
     # 01011900-12312099
     month_day_4_dig_year = create_month_day_4_dig_year(start_year, end_year)
 
     # 000101-991231
-    two_dig_year_month_day = create_2_dig_year_month_day()
+    two_dig_year_month_day = create_2_dig_year_month_day(start_year, end_year)
 
     # 19000101-20991231
     four_dig_year_month_day = create_4_dig_year_month_day(start_year, end_year)
 
     all_dates = two_dig_year + month_day + month_day_2_dig_year + month_day_4_dig_year + two_dig_year_month_day + four_dig_year_month_day
-
-    all_dates = set(list(all_dates))
 
     # Write to rule file
     print("[*] Writing rules...")
@@ -103,34 +101,46 @@ def main():
         	# Dates only
             fh.write('${}\n'.format('$'.join(list(date))))
 
-            # Capitalizes first letter then dates
-            fh.write('c${}\n'.format('$'.join(list(date))))
+            if args.title:
+            	# Capitalizes first letter then dates
+                fh.write('c${}\n'.format('$'.join(list(date))))
 
-            # Capitalizes first letter, doubles, then dates
-            fh.write('cd${}\n'.format('$'.join(list(date))))
+            if args.title and args.double:
+            	# Capitalizes first letter, doubles, then dates
+                fh.write('cd${}\n'.format('$'.join(list(date))))
 
-            # Doubles, dates, then capitalizes first letter
-            fh.write('d${}c\n'.format('$'.join(list(date))))
+            	# Doubles, dates, then capitalizes first letter
+                fh.write('d${}c\n'.format('$'.join(list(date))))
 
             if not special_chars: continue
             for item in special_chars:
-            	# Writes dates with special chars
+                # Writes dates with special chars
                 fh.write('${}${}\n'.format('$'.join(list(date)), '$'.join(list(item))))
 
-                # Writes dates with special chars
-                fh.write('c${}${}\n'.format('$'.join(list(date)), '$'.join(list(item))))
+                if args.title:
+                    # Capitalizes, dates, and writes with special chars
+                    fh.write('c${}${}\n'.format('$'.join(list(date)), '$'.join(list(item))))
 
-                # Capitalizes first letter, doubles, then dates with specials
-                fh.write('cd${}${}\n'.format('$'.join(list(date)), '$'.join(list(item))))
+                if args.double:
+                    # Capitalizes first letter, doubles, then dates with specials
+                    fh.write('cd${}${}\n'.format('$'.join(list(date)), '$'.join(list(item))))
 
-                # Doubles, dates and specials, then capitalizes first letter
-                fh.write('d${}${}c\n'.format('$'.join(list(date)), '$'.join(list(item))))
+                if args.title and args.double:
+                    # Capitalizes first letter, then doubles, dates and specials
+                    fh.write('cd${}${}\n'.format('$'.join(list(date)), '$'.join(list(item))))
+
+                    # Doubles, dates and specials, then capitalizes first letter
+                    fh.write('d${}${}c\n'.format('$'.join(list(date)), '$'.join(list(item))))
+
     print("[+] Complete! Rule file written to {}".format(filename))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verbose",
-                        help="Increase output verbosity",
+    parser.add_argument("-d", "--double",
+                        help="Double word before appending date",
+                        action="store_true")
+    parser.add_argument("-t", "--title",
+                        help="title case word",
                         action="store_true")
     parser.add_argument("-a", "--append_chars",
                         nargs="*",
@@ -139,7 +149,7 @@ if __name__ == '__main__':
                         help="Specify 4 digit year as a starting date range. Default is 1970.")
     parser.add_argument("-e", "--end_year",
                         help="Specify 4 digit year as a starting date range. Default is current year + 1.")
-    parser.add_argument("-f", "--filename",
+    parser.add_argument("-o", "--filename",
                         help="Specify a filename to write to. Default is date_rule.rule")
     args = parser.parse_args()
     if args.start_year:
